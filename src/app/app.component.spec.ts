8a7b5bd81623e931bc4bb800b1c38fc19134cf95
fehmir <fehmir.smaili@uha.fr>
2021-10-28 11:32:45 +0200

    add jenkinsfile

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,17 +1,26 @@
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
+  let http: HttpClient;
+
+  let url = "http://localhost:8080/"
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule
+        RouterTestingModule,
+        HttpClientModule
       ],
       declarations: [
         AppComponent
       ],
     }).compileComponents();
+
+    http = TestBed.inject(HttpClient);
+
   });
 
   it('should create the app', () => {
@@ -19,8 +28,27 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
+  it('should get notes', (done) => {
+    http.get(`${url}/note/all`).subscribe((res: any) => {
+      expect(res.length).toBe(36);
+      done()
+    })
+  });
+  it('should get etudiants', (done) => {
+    http.get(`${url}/etudiant/all`).subscribe((res: any) => {
+      expect(res.length).toBe(6);
+      done()
+    })
+  });
+  it('should get matieres', (done) => {
+    http.get(`${url}/matiere/all`).subscribe((res: any) => {
+      expect(res.length).toBe(6);
+      done()
+    })
+  });
 
-  it(`should have as title 'gestionnairenotes'`, () => {
+
+  /* it(`should have as title 'gestionnairenotes'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('gestionnairenotes');
@@ -31,5 +59,5 @@ describe('AppComponent', () => {
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
     expect(compiled.querySelector('.content span').textContent).toContain('gestionnairenotes app is running!');
-  });
+  }); */
 });
